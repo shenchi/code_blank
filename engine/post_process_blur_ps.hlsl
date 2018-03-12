f1c533ef4afe395495b5aa0b8c4bac7961a76858
xiarui22 <xiarui22@gmail.com>
2018-03-11 22:35:03 -0400

    fix bug

--- engine/post_process_blur_ps.hlsl
+++ engine/post_process_blur_ps.hlsl
@@ -3,25 +3,18 @@ Texture2D brightPart : register(t0);
 float4 main(float4 pos : SV_POSITION) : SV_TARGET{
 
 	float2 uv = pos.xy - 0.5;
-
 	float3 totalColor = float3(0, 0, 0);
-
-	float pixelWidth, pixelHeight;
-	brightPart.GetDimensions(pixelWidth, pixelHeight);
-	pixelWidth = 1 / pixelWidth;
-	pixelHeight = 1 / pixelHeight;
 	uint numSamples = 0;
 	int blurAmount = 5;
 	for (int y = -blurAmount; y <= blurAmount; y++)
 	{
 		for (int x = -blurAmount; x <= blurAmount; x++)
 		{
-			totalColor += brightPart.Load(int3(uv + float2(x * pixelWidth,y * pixelHeight), 0)).rgb;
+			totalColor += brightPart.Load(int3(uv + int2(x ,y), 0)).rgb;
 			numSamples++;
 		}
 	}
 
 	float3 color = totalColor / numSamples;
-
 	return float4(color, 1);
 }
